238583ebd003a00ca81264f2870c873a831537e3
Sayan Deb <sayandeb998@gmail.com>
2024-05-16 17:33:02 +0530

    Latest Code

---
+++ app/Components/SignIn/signIn.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { TextField, Button, Container, Typography, Box } from '@mui/material';
+interface SignInFormProps {
+  onSignIn: () => void;
+}
+const SignInPage: React.FC<SignInFormProps> = ({onSignIn}) => {
+  const [formData, setFormData] = useState({ id: '', password: '' });
+
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Check credentials
+    if (formData.id === 'webskitters' && formData.password === 'webskitters') {
+      onSignIn()
+    } else {
+      alert('Invalid credentials');
+    }
+  };
+
+  return (
+    <Container maxWidth="xs">
+      <Box sx={{ marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="id"
+            label="ID"
+            name="id"
+            autoFocus
+            value={formData.id}
+            onChange={handleChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+            Sign In
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default SignInPage;
---
+++ app/productTable.tsx
@@ -0,0 +1,157 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import authenticate from './Middleware/auth';
+import {
+  Table,
+  Typography,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button,
+} from "@mui/material";
+
+interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  const [selectedRow, setSelectedRow] = useState<number | null>(null);
+  const [selectedCol, setSelectedCol] = useState<number | null>(null);
+  const [openModal, setOpenModal] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setProducts(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const handleClick = (
+    rowIndex: number,
+    colIndex: number,
+    event: React.MouseEvent
+  ) => {
+    event.stopPropagation();
+    setSelectedRow(rowIndex);
+    setSelectedCol(colIndex);
+    setSelectedImage(products[rowIndex].image);
+    setOpenModal(true);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "ArrowUp" && selectedRow !== null && selectedRow > 0) {
+      setSelectedRow(selectedRow - 1);
+      setSelectedImage(products[selectedRow - 1].image);
+    } else if (
+      event.key === "ArrowDown" &&
+      selectedRow !== null &&
+      selectedRow < products.length - 1
+    ) {
+      setSelectedRow(selectedRow + 1);
+      setSelectedImage(products[selectedRow + 1].image);
+    } else if (
+      event.key === "ArrowLeft" &&
+      selectedCol !== null &&
+      selectedCol > 0
+    ) {
+      setSelectedCol(selectedCol - 1);
+    } else if (
+      event.key === "ArrowRight" &&
+      selectedCol !== null &&
+      selectedCol < 4
+    ) {
+      setSelectedCol(selectedCol + 1);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  return (
+    <div>
+      <Typography variant="h4" align="center">
+        Product Table
+      </Typography>
+
+      <TableContainer component={Paper}>
+        <Table
+          aria-label="product table"
+          onClick={() => setSelectedImage(null)}
+          onKeyDown={handleKeyDown}
+          tabIndex={0}
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell>ID</TableCell>
+              <TableCell>Title</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Description</TableCell>
+              <TableCell>Category</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, rowIndex) => (
+              <TableRow key={product.id} selected={selectedRow === rowIndex}>
+                <TableCell>{product.id}</TableCell>
+
+                <TableCell onClick={(e) => handleClick(rowIndex, 1, e)}>
+                  {product.title}
+                </TableCell>
+                <TableCell onClick={(e) => handleClick(rowIndex, 2, e)}>
+                  {product.price}
+                </TableCell>
+                <TableCell onClick={(e) => handleClick(rowIndex, 3, e)}>
+                  {product.description}
+                </TableCell>
+                <TableCell onClick={(e) => handleClick(rowIndex, 4, e)}>
+                  {product.category}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Selected Image</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {selectedImage && (
+              <img
+                src={selectedImage}
+                alt="Selected Product"
+                style={{ width: "500px", height:'500px'  }}
+              />
+            )}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
